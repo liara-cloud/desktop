375251b6099f802e3cee196104157b5ac04eaa91
ali gholi <aligholi8220@gmail.com>
2021-12-22 09:23:44 +0330

    Remove Project elements

--- main/util/hash.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto'); 
-
-const hash = buf => {
-  return crypto.createHash('sha256').update(buf).digest('hex');
-};
-
-module.exports = hash;
--- renderer/next.config.js
+++
@@ -1,7 +0,0 @@
-exports.webpack = config => Object.assign(config, {
-  target: 'electron-renderer'
-})
-
-exports.exportPathMap = () => ({
-  '/start': { page: '/start' }
-})
