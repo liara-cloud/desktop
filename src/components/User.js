3abff90e218dbf0c244878191c4aaa35b44ec1ba
ali gholi <aligholi8220@gmail.com>
2021-12-27 10:08:19 +0330

    Fix(style) : Disorder in Draggable & menu

--- src/components/User.js
+++ src/components/User.js
@@ -24,28 +24,31 @@ const User = ({ setShowApps }) => {
           </span>
         </div>
         {menu && (
-          <div className="menu">
-            <div className="user-item carrent" style={{ margin: 0 }}>
-              <img src={person} />
-              <span className="region">
-                <Iran />
-              </span>
-              <p>نام و نام خانوادگی</p>
+          <>
+            <div className="menu">
+              <div className="user-item carrent" style={{ margin: 0 }}>
+                <img src={person} />
+                <span className="region">
+                  <Iran />
+                </span>
+                <p>نام و نام خانوادگی</p>
+              </div>
+              <div className="user-item menu-item" style={{ margin: 0 }}>
+                <img src={person} />
+                <p>نام و نام خانوادگی</p>
+                <span className="region">
+                  <German />
+                </span>
+              </div>
+              <a className="add" href="#">
+                افزودن حساب کاربری
+              </a>
+              <a className="exit" href="/SelectApps">
+                خروج
+              </a>
             </div>
-            <div className="user-item menu-item" style={{ margin: 0 }}>
-              <img src={person} />
-              <p>نام و نام خانوادگی</p>
-              <span className="region">
-                <German />
-              </span>
-            </div>
-            <a className="add" href="#">
-              افزودن حساب کاربری
-            </a>
-            <a className="exit" href="/SelectApps">
-              خروج
-            </a>
-          </div>
+            <div className="menu-back" onClick={() => setMenu(false)}></div>
+          </>
         )}
       </div>
     </>
