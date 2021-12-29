f533787feff751ebe861312a3162b64efabaaffd
ali gholi <aligholi8220@gmail.com>
2021-12-29 10:15:07 +0330

    fix : Redirect in Splash.js

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,16 +1,17 @@
 import { ipcRenderer } from "electron";
 import React, { Component } from "react";
+import { withRouter } from "react-router";
+import Redirect from "react-router/Redirect";
 
 class Splash extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      accounts: "",
+      accounts: [],
       current: "",
-      account: "",
+      account: null,
     };
   }
-
   componentDidMount() {
     ipcRenderer.on("asynchronous-login", (event, arg) => {
       if (arg.accounts !== undefined) {
@@ -19,18 +20,24 @@ class Splash extends Component {
           current: arg.current,
         });
       } else {
-        console.log(arg);
         this.setState({ ...this.state, account: arg });
       }
     });
-
     ipcRenderer.send("asynchronous-login", "liara-cloud");
+    if (this.state.accounts.length === 0 && this.state.account === null) {
+      console.log("liara");
+      this.props.history.push("/login");
+    }
+    // if (this.state.accounts !== [] && this.state.account !== {}) {
+    //   console.log("hello");
+    //   // this.props.history.push("/Draggable");
+    //   console.log(this.state.accounts , this.state.account);
+    // }
   }
-
+  // componentWillUnmount() {}
   render() {
-    console.log(this.state.account, this.state.accounts);
     return <div style={{ textAlign: "center", marginTop: "50%" }}>splash</div>;
   }
 }
 
-export default Splash;
+export default withRouter(Splash);
