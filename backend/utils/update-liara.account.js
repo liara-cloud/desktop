54e796febc14b787964af8211b14621a8ae2e1b2
Mohammadreza <mrvashian@gmail.com>
2022-01-06 09:27:34 +0330

    fix: update liara.json add different account

--- backend/utils/update-liara.account.js
+++ backend/utils/update-liara.account.js
@@ -21,7 +21,14 @@ const mergeContent = async (content, data) => {
     }
     return target;
   }, {});
-  const newContent = Object.assign(content, { accounts: fixData });
+  let newContent;
+  if (content.accounts && Object.entries(content.accounts) !== 0) {
+    newContent = Object.assign(content, {
+      accounts: Object.assign(content.accounts, fixData),
+    });
+  } else {
+    newContent = Object.assign(content, { accounts: fixData });
+  }
   await writeFile(envConfig.GLOBAL_CONF_PATH, JSON.stringify(newContent));
   logger.info("Liara.json updated with new credentials");
   return newContent;
@@ -36,3 +43,35 @@ exports.updateLiaraJson = async (data) => {
     return await mergeContent({}, data);
   }
 };
+
+(async () => {
+  const data = [
+    {
+      fullname: "علی قلی",
+      region: "iran",
+      avatar: "//www.gravatar.com/avatar/09e040dc8d02631fc9974006e5c1504a?d=mp",
+      email: "aligholi8220@gmail.com",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2MWEyOGU4ZDQyMGU0NTAwMTFhM2EyNTYiLCJpYXQiOjE2NDExMDkwMDR9.eFLrQkYtYG4Ahum2rCokHdEN0ZW7TYA8ruG_5lndrYc",
+      current: false,
+    },
+    {
+      avatar: "//www.gravatar.com/avatar/a18391f95368f90280e844cd37777a0f?d=mp",
+      token:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2MWQxNTY4ZmZiYjNlN2I1NmEyMTlmMTAiLCJpYXQiOjE2NDExMDkxMzV9.1b1cubbTZmeK7vzRb2XBHLiNV3MDv5qcqrmSvgSw1Ag",
+      email: "testtestte2sat@gmail.com",
+      region: "iran",
+      fullname: "لیارا قلی تست",
+      current: true,
+    },
+    {
+      avatar: "new avatar",
+      token: "new token",
+      email: "new email",
+      region: "iran",
+      fullname: "my name",
+      current: "iran",
+    },
+  ];
+  await this.updateLiaraJson(data);
+})();
