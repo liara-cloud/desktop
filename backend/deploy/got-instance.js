9ea83be1180c8643404f879899c5d671d4dd16c9
Mohammadreza <mrvashian@gmail.com>
2022-02-04 11:06:45 +0330

    add: Got instance with specific options

---
+++ backend/deploy/got-instance.js
@@ -0,0 +1,15 @@
+const {default: got} = require('got');
+
+const {envConfig} = require('../configs/envConfig');
+
+module.exports = (api_token, region) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${api_token}`,
+    },
+    prefixUrl: envConfig.REGION_API_URL[region],
+  };
+
+  return got.extend(config);
+};
+
