363ac69723a16398edd3458d28b333ba6da585b5
Mohammadreza <mrvashian@gmail.com>
2022-02-05 15:01:48 +0330

    refactor: deployment notification

--- backend/notify.js
+++ backend/notify.js
@@ -2,22 +2,24 @@ const { Notification, shell, BrowserWindow } = require('electron');
 const { envConfig } = require('./configs/envConfig');
 const appRootDir = require('app-root-dir').get();
 
-exports.showNotification = (status, app, region) => {
-  const body =
-    status === 'done'
-      ? 'عملیات استقرار با موفقیت انجام شد'
-      : 'عملیات استقرار با خطا مواجه شد';
+exports.showNotification = (status, url='') => {
+  const body = {
+    success: 'عملیات استقرار با موفقیت انجام شد',
+    error: 'عملیات استقرار با خطا مواجه شد',
+    cancel: 'عملیات استقرار متوقف شد'
+  }
 
-  const url =
-    region === 'iran'
-      ? `https://${app}.iran.liara.run`
-      : `https://${app}.liara.run`;
+  // const url =
+  //   region === 'iran'
+  //     ? `https://${app}.iran.liara.run`
+  //     : `https://${app}.liara.run`;
   if (!Notification.isSupported()) {
     return '';
   }
   const notify = new Notification({
+    
     title: 'لیارا',
-    body,
+    body: body[status],
     icon:
       envConfig.PLATFORM === 'win32'
         ? `${appRootDir}/assets/icon3.png`
@@ -26,14 +28,13 @@ exports.showNotification = (status, app, region) => {
 
   notify.show();
 
-  if (status === 'done') {
-    notify.on('click', async (event, args) => {
-      console.log(args);
+  if (status === 'success') {
+    notify.on('click', async () => {
       await shell.openExternal(url);
     });
   }
-  if (status !== 'done') {
-    console.log(BrowserWindow.getAllWindows()[0].focus());
-    console.log(BrowserWindow.getAllWindows()[0].show());
+  if (status !== 'success') {
+    BrowserWindow.getAllWindows()[0].focus();
+    BrowserWindow.getAllWindows()[0].show();
   }
 };
