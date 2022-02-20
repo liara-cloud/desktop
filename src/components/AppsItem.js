ed36299574733d3e14b3d0d2028a4b68a7842d8b
ali gholi <aligholi8220@gmail.com>
2022-02-20 10:16:56 +0330

    Remove platformIcon directory

--- src/components/AppsItem.js
+++ src/components/AppsItem.js
@@ -9,10 +9,6 @@ export default function AppsItem({ item, setSelected, setShowApps }) {
       key={item.name}
       onClick={() => setSelected(item) + setShowApps(false)}
     >
-      {/* <img
-        className="icon-platform"
-        src={require(`@liara/platformicons/svg/${item.type}.svg`).default}
-      /> */} 
      <PlatformIcon platform={item.type} />
       {item.project_id}
     </div>
