3e32e53e5a79e35dccb266e119ba2945688bdc69
Mohammadreza <mrvashian@gmail.com>
2022-02-20 16:55:43 +0330

    fix: delete account if it has fake token

--- backend/utils/account-management.js
+++ backend/utils/account-management.js
@@ -1,12 +1,12 @@
-const logger = require("../configs/logger");
-const { getUser } = require("./get-account");
-const { readFile, writeFile } = require("fs-extra");
-const { envConfig } = require("../configs/envConfig");
+const logger = require('../configs/logger');
+const { getUser } = require('./get-account');
+const { readFile, writeFile } = require('fs-extra');
+const { envConfig } = require('../configs/envConfig');
 
 exports.readLiaraJson = async () => {
+  let content;
   try {
-    const content =
-      JSON.parse(await readFile(envConfig.GLOBAL_CONF_PATH)) || {};
+    content = JSON.parse(await readFile(envConfig.GLOBAL_CONF_PATH)) || {};
 
     if (
       content.region &&
@@ -14,7 +14,7 @@ exports.readLiaraJson = async () => {
       (!content.accounts || !Object.keys(content.accounts).length)
     ) {
       const user = await getUser(content.api_token, content.region);
-      const accountName = `${user.email.split("@")[0]}_${content.region}`;
+      const accountName = `${user.email.split('@')[0]}_${content.region}`;
       const account = {
         [accountName]: {
           email: user.email,
@@ -49,7 +49,7 @@ exports.readLiaraJson = async () => {
               region: value.region,
               current: value.current,
               fullname: user.fullname,
-              api_token: user.accessToken
+              api_token: user.accessToken,
             },
           };
 
@@ -63,7 +63,30 @@ exports.readLiaraJson = async () => {
     }
     return [];
   } catch (error) {
-    logger.error(error)
+    logger.error(error);
+    if (error.message.startsWith('{"apiToken')) {
+      content.accounts &&
+        Object.keys(content.accounts).map(async (account) => {
+          if (
+            content.accounts[account].api_token ===
+              JSON.parse(error.message).apiToken &&
+            content.accounts[account].region === JSON.parse(error.message).region
+          ) {
+            content.current =
+              content.current === account ? null : content.current;
+            delete content.accounts[account];
+          }
+        });
+      if (
+        content.api_token === JSON.parse(error.message).apiToken &&
+        content.region === JSON.parse(error.message).region
+      ) {
+        delete content.api_token;
+        delete content.region;
+      }
+      await writeFile(envConfig.GLOBAL_CONF_PATH, JSON.stringify(content));
+      return await this.readLiaraJson();
+    }
     if (error.message === 'TIMEOUT') {
       logger.error('Get user data time out. It took about 10 seconds');
       return [];
@@ -72,79 +95,3 @@ exports.readLiaraJson = async () => {
     return [];
   }
 };
-
-// this.readLiaraJson().then(console.log);
-
-// const contentKeys = Object.keys(content);
-// if (
-//   (!contentKeys.includes('accounts') ||
-//     Object.keys(content.accounts).length == 0) &&
-//   contentKeys.includes('api_token') &&
-//   contentKeys.includes('region')
-// )
-
-// account[accountName] = {};
-// account[accountName]['fullname'] = user.fullname;
-// account[accountName]['email'] = user.email;
-// account[accountName]['avatar'] = user.avatar;
-// account[accountName]['region'] = content.region;
-// account[accountName]['api_token'] = content.api_token;
-// account[accountName]['current'] = true;
-// content.current = accountName;
-// content['accounts'] = account;
-// await writeFile(envConfig.GLOBAL_CONF_PATH, JSON.stringify(content));
-
-// for (const [key, value] of Object.entries(content.accounts)) {
-//   console.log(key);
-//   console.log(value);
-//   const user = await getUser(value.api_token, value.region);
-//   const accountName = `${user.email.split('@')[0]}_${value.region}`;
-//   value.fullname = user.fullname;
-//   value.avatar = user.avatar;
-//   value.current = content.current === key ? true : false;
-
-//   hasCurrent && hasCurrent === key ? true : (hasCurrent = key);
-
-//   if (hasCurrent) {
-//     value.current = hasCurrent === key ? true : false;
-//   }
-//   if (!hasCurrent) {
-//     value.current = true;
-//     hasCurrent = key;
-//   }
-//   // if (hasCurrent && content.current === key) {
-//   //   value.current = true;
-//   // }
-//   // content.accounts[key]['fullname'] = user.fullname;
-//   // content.accounts[key]['avatar'] = user.avatar;
-//   // if (!value.current) {
-//   //   value.current = false;
-//   // }
-//   // if (content.current === key) {
-//   //   value.current = true;
-//   // }
-//   // if (!content.accounts[key]['current']) {
-//   //   content.accounts[key]['current'] = false;
-//   //   if (content.current == key) {
-//   //     content.accounts[key]['current'] = true;
-//   //     content.current = accountName;
-//   //     content.api_token = user.api_token;
-//   //   }
-//   // }
-//   // content.accounts[accountName] = content.accounts[key];
-//   // if (key !== accountName) {
-//   //   delete content.accounts[key];
-//   // }
-// }
-// let hasCurrent = false;
-// for (const [key, value] of Object.entries(content.accounts)) {
-//   if (value.current) {
-//     hasCurrent = true;
-//   }
-// }
-// if (!hasCurrent) {
-//   Object.values(content.accounts)[0].current = true;
-// }
-// return {
-//   accounts: content.accounts,
-// };
--- backend/utils/get-account.js
+++ backend/utils/get-account.js
@@ -2,12 +2,19 @@ const logger = require('../configs/logger');
 const gotInstance = require('../deploy/got-instance');
 
 exports.getUser = async (apiToken, region) => {
-  logger.info('Start to get user data');
-  const response = await gotInstance(apiToken, region)
-    .get('v1/me', {
-      timeout: 10 * 1000,
-    })
-    .json();
-  logger.info(`Get User Data: ${response.user}`);
-  return response.user;
+  try {
+    logger.info('Start to get user data');
+    const response = await gotInstance(apiToken, region)
+      .get('v1/me', {
+        timeout: 10 * 1000,
+      })
+      .json();
+    logger.info(`Get User Data: ${response.user}`);
+    return response.user;
+  } catch (error) {
+    if (error.response.statusMessage !== 'Unauthorized') {
+      throw err;
+    }
+    throw new Error(JSON.stringify({ apiToken, region }));
+  }
 };
