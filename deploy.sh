3b65e5241abcaad0172457fa1840db01db0c43c9
Mohammadreza <mrvashian@gmail.com>
2022-03-09 14:07:45 +0330

    fix: prevent push unnecessary package to s3

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # build the app
-# npm run build
-# npm run builder
+npm run build
+npm run builder
 
 cd release
 
 # create zip files
 for filename in Liara-Desktop*; do
+  if [[ ${filename} =~ (zip|win.exe)$ ]]; then
+    rm ${filename}
+    continue
+  fi
   if [[ ${filename} =~ (exe|deb|dmg)$ ]]; then
     zip "${filename}.zip" $filename
     c=$(echo "$filename".zip | cut -d- -f4-)
-    copy "${filename}.zip" "Liara-Desktop-latest-$c"
+    cp "${filename}.zip" "Liara-Desktop-latest-$c"
+    rm $filename
   fi
 done
 
-# upload to minio
+# # upload to minio
 mc cp -r latest* "${MINIO_TARGET}"/desktop 
 mc cp -r Liara-Desktop* "${MINIO_TARGET}"/desktop 
