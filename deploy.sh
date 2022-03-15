8b30a0c8afcf70212ac1dd37acfeb2669ba63059
Mohammadreza <mrvashian@gmail.com>
2022-03-15 15:27:19 +0330

    add: github action workflow to build and deploy app

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -e
+
+# download mc client 
+wget https://dl.min.io/client/mc/release/linux-amd64/mc
+chmod +x ./mc
+
+# set alias
+./mc alias set "${AWS_TARGET}" "${AWS_ENDPOINT}" "${AWS_ACCESS_KEY_ID}" "${AWS_SECRET_ACCESS_KEY}"
+
 # build the app
 npm run build
 npm run builder
@@ -8,18 +17,13 @@ cd release
 
 # create zip files
 for filename in Liara-Desktop*; do
-  if [[ ${filename} =~ (zip|win.exe)$ ]]; then
-    rm ${filename}
-    continue
-  fi
   if [[ ${filename} =~ (exe|deb|dmg)$ ]]; then
     zip "${filename}.zip" $filename
     c=$(echo "$filename".zip | cut -d- -f4-)
     cp "${filename}.zip" "Liara-Desktop-latest-$c"
-    rm $filename
   fi
 done
 
-# # upload to minio
-mc cp -r latest* "${MINIO_TARGET}"/desktop 
-mc cp -r Liara-Desktop* "${MINIO_TARGET}"/desktop 
+# upload to minio
+../mc cp -r latest* "${AWS_TARGET}"/releases 
+../mc cp -r Liara-Desktop* "${AWS_TARGET}"/releases 
