922a32ada514ec025b9271ac6f3d5c764d938a84
gholi-dev <aligholi8220@gmail.com>
2022-07-03 11:36:56 +0430

    fix setPort

--- src/components/AppsItem.js
+++ src/components/AppsItem.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "./contextApi/Context";
 import PlatformIcon from "./PlatformIcon";
 
 export default function AppsItem({ item, setSelected, setShowApps }) {
+  const { setPort } = useContext(Context);
+
+  const handleSwithApp = () => {
+    setSelected(item);
+    setShowApps(false);
+    setPort("");
+  };
+
   return (
-    <div
-      key={item.project_id}
-      className="item"
-      onClick={() => setSelected(item) + setShowApps(false)}
-    >
+    <div key={item.project_id} className="item" onClick={handleSwithApp}>
       <PlatformIcon platform={item.type} />
       {item.project_id}
     </div>
