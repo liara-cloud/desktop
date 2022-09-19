f925ead0488f74358f6c6428bf16866453a53bf6
gholi-dev <aligholi8220@gmail.com>
2022-09-19 16:31:17 +0430

    Add scrollToBottom for Log-container

--- src/components/upload-info/upload-info.component.js
+++ src/components/upload-info/upload-info.component.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { InfoContainer } from "./upload-info.style";
 
 const UploadInfo = ({ as = "pre", log, children, ...otherProps }) => {
+  const preRef = useRef();
+
+  useEffect(() => {
+    if (!preRef.current) {
+      return null;
+    }
+    const isScrolledToBottom =
+      preRef.current.scrollHeight - preRef.current.clientHeight <=
+      preRef.current.scrollTop + 1;
+    if (!isScrolledToBottom) {
+      preRef.current.scrollTop =
+        preRef.current.scrollHeight - preRef.current.clientHeight;
+    }
+  });
+
   if (as == "div") {
     return (
       <InfoContainer as={as} {...otherProps}>
@@ -13,8 +28,9 @@ const UploadInfo = ({ as = "pre", log, children, ...otherProps }) => {
   return (
     <InfoContainer
       dangerouslySetInnerHTML={{ __html: log }}
+      ref={preRef}
       {...otherProps}
-    ></InfoContainer>
+    />
   );
 };
 
