27f780487b90ecc31eefd5b15a66985110380924
Mohammad Hossein Ebadollahi <mhemrg120@gmail.com>
2022-10-09 16:16:20 +0330

    Add app version to Sentry

--- main/configs/sentry.js
+++ main/configs/sentry.js
@@ -1,5 +1,8 @@
 const Raven = require("raven");
+const pkg = require('../../package.json');
 
 exports.sentry = Raven.config(
   "https://741a1b1949d749558159bfc1b7e95878@sentry.liara.ir/15"
-).install();
+, {
+  release: pkg.version,
+}).install();
